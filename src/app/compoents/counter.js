8f484232dc5ecd015f4358a0bb851fa73e9061eb
Feng, Cheng-Chi <qwe987299@gmail.com>
2019-10-06 16:03:34 +0800

    第三版(下課前)

---
+++ src/app/compoents/counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+var Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Counter">
+      <h1>Hello CodeSandbox</h1>
+      <h2>You clicked {count} times!</h2>
+
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default Counter
--- src/app/react.jsx
+++ src/app/react.jsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './compoents/counter'
 
-var App = () => {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>You clicked {count} times!</h2>
-
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  )
-}
-
+const MainScreen = () => <div className="alert">
+  <h3>計數器</h3>
+  <App />
+</div>
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<MainScreen />, rootElement)
